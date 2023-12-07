4802bd5d01963b7649f3e48581d8fbd0be1612c7
Brendan Busch <brendan.busch@uwa.edu.au>
2023-12-07 17:08:31 +0800

    Updated Grids

--- grids/layers/layers.js
+++ grids/layers/layers.js
@@ -26,52 +26,49 @@ var lyr_B009_1 = new ol.layer.Vector({
                 interactive: true,
                 title: '<img src="styles/legend/B009_1.png" /> B009'
             });
-var format_A001_2 = new ol.format.GeoJSON();
-var features_A001_2 = format_A001_2.readFeatures(json_A001_2, 
+var format_C001_2 = new ol.format.GeoJSON();
+var features_C001_2 = format_C001_2.readFeatures(json_C001_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_A001_2 = new ol.source.Vector({
+var jsonSource_C001_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_A001_2.addFeatures(features_A001_2);
-var lyr_A001_2 = new ol.layer.Vector({
+jsonSource_C001_2.addFeatures(features_C001_2);
+var lyr_C001_2 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_A001_2, 
-                style: style_A001_2,
+                source:jsonSource_C001_2, 
+                style: style_C001_2,
                 interactive: true,
-                title: '<img src="styles/legend/A001_2.png" /> A001'
+                title: '<img src="styles/legend/C001_2.png" /> C001'
             });
-var format_C001_3 = new ol.format.GeoJSON();
-var features_C001_3 = format_C001_3.readFeatures(json_C001_3, 
+var format_A001_3 = new ol.format.GeoJSON();
+var features_A001_3 = format_A001_3.readFeatures(json_A001_3, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_C001_3 = new ol.source.Vector({
+var jsonSource_A001_3 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_C001_3.addFeatures(features_C001_3);
-var lyr_C001_3 = new ol.layer.Vector({
+jsonSource_A001_3.addFeatures(features_A001_3);
+var lyr_A001_3 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_C001_3, 
-                style: style_C001_3,
+                source:jsonSource_A001_3, 
+                style: style_A001_3,
                 interactive: true,
-                title: '<img src="styles/legend/C001_3.png" /> C001'
+                title: '<img src="styles/legend/A001_3.png" /> A001'
             });
-var group_Domains = new ol.layer.Group({
-                                layers: [lyr_B009_1,lyr_A001_2,lyr_C001_3,],
-                                title: "Domains"});
 var group_Basemaps = new ol.layer.Group({
                                 layers: [lyr_ESRIGraylight_0,],
                                 title: "Basemaps"});
 
-lyr_ESRIGraylight_0.setVisible(true);lyr_B009_1.setVisible(true);lyr_A001_2.setVisible(true);lyr_C001_3.setVisible(true);
-var layersList = [group_Basemaps,group_Domains];
+lyr_ESRIGraylight_0.setVisible(true);lyr_B009_1.setVisible(true);lyr_C001_2.setVisible(true);lyr_A001_3.setVisible(true);
+var layersList = [group_Basemaps,lyr_B009_1,lyr_C001_2,lyr_A001_3];
 lyr_B009_1.set('fieldAliases', {'FID': 'FID', });
-lyr_A001_2.set('fieldAliases', {'FID': 'FID', });
-lyr_C001_3.set('fieldAliases', {'FID': 'FID', });
+lyr_C001_2.set('fieldAliases', {'FID': 'FID', });
+lyr_A001_3.set('fieldAliases', {'FID': 'FID', });
 lyr_B009_1.set('fieldImages', {'FID': 'TextEdit', });
-lyr_A001_2.set('fieldImages', {'FID': 'TextEdit', });
-lyr_C001_3.set('fieldImages', {'FID': 'TextEdit', });
-lyr_B009_1.set('fieldLabels', {});
-lyr_A001_2.set('fieldLabels', {});
-lyr_C001_3.set('fieldLabels', {});
-lyr_C001_3.on('precompose', function(evt) {
+lyr_C001_2.set('fieldImages', {'FID': 'TextEdit', });
+lyr_A001_3.set('fieldImages', {'FID': 'TextEdit', });
+lyr_B009_1.set('fieldLabels', {'FID': 'no label', });
+lyr_C001_2.set('fieldLabels', {'FID': 'no label', });
+lyr_A001_3.set('fieldLabels', {'FID': 'no label', });
+lyr_A001_3.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
