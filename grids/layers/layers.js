38e2c322b2ccea91cd4ac0d480f6477cd7b069b5
Brendan Busch <brendan.busch@uwa.edu.au>
2023-12-07 17:39:20 +0800

    grid test

--- grids/layers/layers.js
+++ grids/layers/layers.js
@@ -12,63 +12,39 @@ var wms_layers = [];
                 url: 'http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'
             })
         });
-var format_B009_1 = new ol.format.GeoJSON();
-var features_B009_1 = format_B009_1.readFeatures(json_B009_1, 
+var format_csiem_mesh_A001_lo_1 = new ol.format.GeoJSON();
+var features_csiem_mesh_A001_lo_1 = format_csiem_mesh_A001_lo_1.readFeatures(json_csiem_mesh_A001_lo_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_B009_1 = new ol.source.Vector({
+var jsonSource_csiem_mesh_A001_lo_1 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_B009_1.addFeatures(features_B009_1);
-var lyr_B009_1 = new ol.layer.Vector({
+jsonSource_csiem_mesh_A001_lo_1.addFeatures(features_csiem_mesh_A001_lo_1);
+var lyr_csiem_mesh_A001_lo_1 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_B009_1, 
-                style: style_B009_1,
+                source:jsonSource_csiem_mesh_A001_lo_1, 
+                style: style_csiem_mesh_A001_lo_1,
                 interactive: true,
-                title: '<img src="styles/legend/B009_1.png" /> B009'
-            });
-var format_C001_2 = new ol.format.GeoJSON();
-var features_C001_2 = format_C001_2.readFeatures(json_C001_2, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_C001_2 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_C001_2.addFeatures(features_C001_2);
-var lyr_C001_2 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_C001_2, 
-                style: style_C001_2,
-                interactive: true,
-                title: '<img src="styles/legend/C001_2.png" /> C001'
-            });
-var format_A001_3 = new ol.format.GeoJSON();
-var features_A001_3 = format_A001_3.readFeatures(json_A001_3, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_A001_3 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_A001_3.addFeatures(features_A001_3);
-var lyr_A001_3 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_A001_3, 
-                style: style_A001_3,
-                interactive: true,
-                title: '<img src="styles/legend/A001_3.png" /> A001'
-            });
+    title: 'csiem_mesh_A001_lo<br />\
+    <img src="styles/legend/csiem_mesh_A001_lo_1_0.png" /> -108 - -97<br />\
+    <img src="styles/legend/csiem_mesh_A001_lo_1_1.png" /> -97 - -86<br />\
+    <img src="styles/legend/csiem_mesh_A001_lo_1_2.png" /> -86 - -74<br />\
+    <img src="styles/legend/csiem_mesh_A001_lo_1_3.png" /> -74 - -63<br />\
+    <img src="styles/legend/csiem_mesh_A001_lo_1_4.png" /> -63 - -52<br />\
+    <img src="styles/legend/csiem_mesh_A001_lo_1_5.png" /> -52 - -41<br />\
+    <img src="styles/legend/csiem_mesh_A001_lo_1_6.png" /> -41 - -30<br />\
+    <img src="styles/legend/csiem_mesh_A001_lo_1_7.png" /> -30 - -18<br />\
+    <img src="styles/legend/csiem_mesh_A001_lo_1_8.png" /> -18 - -7<br />\
+    <img src="styles/legend/csiem_mesh_A001_lo_1_9.png" /> -7 - 4<br />'
+        });
 var group_Basemaps = new ol.layer.Group({
                                 layers: [lyr_ESRIGraylight_0,],
                                 title: "Basemaps"});
 
-lyr_ESRIGraylight_0.setVisible(true);lyr_B009_1.setVisible(true);lyr_C001_2.setVisible(true);lyr_A001_3.setVisible(true);
-var layersList = [group_Basemaps,lyr_B009_1,lyr_C001_2,lyr_A001_3];
-lyr_B009_1.set('fieldAliases', {'FID': 'FID', });
-lyr_C001_2.set('fieldAliases', {'FID': 'FID', });
-lyr_A001_3.set('fieldAliases', {'FID': 'FID', });
-lyr_B009_1.set('fieldImages', {'FID': 'TextEdit', });
-lyr_C001_2.set('fieldImages', {'FID': 'TextEdit', });
-lyr_A001_3.set('fieldImages', {'FID': 'TextEdit', });
-lyr_B009_1.set('fieldLabels', {'FID': 'no label', });
-lyr_C001_2.set('fieldLabels', {'FID': 'no label', });
-lyr_A001_3.set('fieldLabels', {'FID': 'no label', });
-lyr_A001_3.on('precompose', function(evt) {
+lyr_ESRIGraylight_0.setVisible(true);lyr_csiem_mesh_A001_lo_1.setVisible(true);
+var layersList = [group_Basemaps,lyr_csiem_mesh_A001_lo_1];
+lyr_csiem_mesh_A001_lo_1.set('fieldAliases', {'Z': 'Z', 'Mat_Zone': 'Mat_Zone', });
+lyr_csiem_mesh_A001_lo_1.set('fieldImages', {'Z': 'TextEdit', 'Mat_Zone': 'Range', });
+lyr_csiem_mesh_A001_lo_1.set('fieldLabels', {'Z': 'no label', 'Mat_Zone': 'no label', });
+lyr_csiem_mesh_A001_lo_1.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
